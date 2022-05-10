661e0030c05c03282d48d36af6d0e7b8e6981eef
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2022-05-10 14:12:12 +0300

    Fix token expiration

--- app/oauth2.py
+++ app/oauth2.py
@@ -22,7 +22,7 @@ def create_access_token(data: dict) -> str:
     to_encode = data.copy()
 
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({'expire_at': int(expire.timestamp())})
+    to_encode.update({'exp': expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
