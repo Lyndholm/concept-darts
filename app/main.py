c8e504645a56a4e1f3a922bc75764f177fa6d9c6
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2022-05-11 19:14:25 +0300

    CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import views
 
 
 app = FastAPI(title='ITForDesigners')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 @app.on_event('startup')
 async def startup():
